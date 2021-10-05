aa778eda7ab839dba64cddb0d14311c3cdc39299
Vaibhav Mittal <53215737+mittalvaibhav1@users.noreply.github.com>
2021-10-05 23:32:18 +0530

    Kadane's Algorithm (#1)

---
+++ geeksforgeeks/Kadane's Algorithm.java
@@ -0,0 +1,15 @@
+//https://practice.geeksforgeeks.org/problems/kadanes-algorithm/1
+
+class Kadane{
+    
+    int maxSubarraySum(int arr[], int n){
+        int maxSum = arr[0];
+        int currSum = arr[0];
+        for(int i = 1; i < n ; i++) {
+            currSum = Math.max(currSum + arr[i], arr[i]);
+            maxSum = Math.max(currSum, maxSum);
+        }
+        return maxSum;
+    }
+    
+}
